05cabcea30566196fe8a55d8e78c8748c4b8b81b
mateussousaa <mateussousa20@outlook.com>
2022-11-23 17:31:42 -0300

    feat: making the migration to create users table & user model - requirement 01

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,53 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  // underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default User;
