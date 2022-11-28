d036d649546fbc93ee65e8a15cf45946e4ad1ce7
mateussousaa <mateussousa20@outlook.com>
2022-11-28 19:44:03 -0300

    test: creating the tests of teams route

---
+++ app/backend/src/tests/team.route.test.ts
@@ -0,0 +1,60 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import App from '../app';
+import Example from '../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+import mockedTeams from './mocks/mockedTeams';
+import Team from '../database/models/Team';
+
+chai.use(chaiHttp);
+
+const { app } = new App();
+
+const { expect } = chai;
+
+describe('Seu teste', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(Team, "findAll")
+      .resolves(mockedTeams as Team[]);
+  });
+
+  after(()=>{
+    (Team.findAll as sinon.SinonStub).restore();
+  })
+
+  it('should test the endpoint /teams', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/teams')
+
+    expect(chaiHttpResponse.status).to.be.equal(200)
+    expect(chaiHttpResponse.body).to.be.deep.equal(mockedTeams)
+  });
+
+  before(async () => {
+    sinon
+      .stub(Team, "findByPk")
+      .resolves(mockedTeams[1] as Team);
+  });
+
+  after(()=>{
+    (Team.findByPk as sinon.SinonStub).restore();
+  })
+
+  it('should test the endpoint /teams/:id', async () => {    
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/teams/2')
+
+    expect(chaiHttpResponse.status).to.be.equal(200)
+    expect(chaiHttpResponse.body).to.be.deep.equal(mockedTeams[1])
+  });
+
+});
