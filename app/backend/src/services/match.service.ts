13ceb04ce66d401a004e7b622aa9a602487eac7e
mateussousaa <mateussousa20@outlook.com>
2022-11-29 18:22:45 -0300

    feat: creating the body request validation when the teams ids are invalid - route matches/ [POST] - requirement 26

--- app/backend/src/services/match.service.ts
+++ app/backend/src/services/match.service.ts
@@ -1,6 +1,7 @@
 import Team from '../database/models/Team';
 import Match from '../database/models/Match';
 import IMatchForCreation from '../interfaces/IMatch';
+import HttpException from '../utils/HttpException';
 
 export default class MatchService {
   getAllMatches = async () => Match.findAll({
@@ -10,7 +11,15 @@ export default class MatchService {
     ],
   });
 
-  createMatch = async (match: IMatchForCreation) => Match.create({ ...match, inProgress: true });
+  createMatch = async (match: IMatchForCreation) => {
+    const homeTeam = await Match.findByPk(match.homeTeam);
+    if (!homeTeam) throw new HttpException(404, 'There is no team with such id!');
+
+    const awayTeam = await Match.findByPk(match.awayTeam);
+    if (!awayTeam) throw new HttpException(404, 'There is no team with such id!');
+
+    return Match.create({ ...match, inProgress: true });
+  };
 
   finishMatch = async (id: number) => Match.update({ inProgress: false }, { where: { id } });
 }
