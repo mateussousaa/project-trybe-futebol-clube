ced0c22ce81f6ce263d42136258d1322e1a1727f
mateussousaa <mateussousa20@outlook.com>
2022-11-29 15:52:24 -0300

    test: creating the endpoint GET - /matches?inProgress=

--- app/backend/src/controllers/match.controller.ts
+++ app/backend/src/controllers/match.controller.ts
@@ -4,7 +4,11 @@ import MatchService from '../services/match.service';
 export default class MatchController {
   constructor(private _service = new MatchService()) {}
   getAllMatches: RequestHandler = async (req, res) => {
-    const matches = await this._service.getAllMatches();
+    const { inProgress } = req.query;
+    let matches = await this._service.getAllMatches();
+    if (inProgress === 'true') {
+      matches = matches.filter((match) => match.inProgress);
+    }
     res.status(200).json(matches);
   };
 }
